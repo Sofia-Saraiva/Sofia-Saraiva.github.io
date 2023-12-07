bb5603ac0a444c2c96b07f5a1cd0f57cadd018ee
Sofia Saraiva <112433998+Sofia-Saraiva@users.noreply.github.com>
2023-12-07 00:28:51 -0300

    adicao de contact, footer, etc

--- static/script.js
+++ static/script.js
@@ -1,22 +1,24 @@
 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+    window.onscroll = () => {
 
-        if (top >= offset && top < offset + height) {
-            // Remove 'active' class from all navigation links
-            navLinks.forEach(link => {
-                link.classList.remove('active');
-            });
+        sections.forEach(sec => {
 
-            // Add 'active' class to the corresponding navigation link
-            document.querySelector('nav a[href*=' + id + ']').classList.add('active');
-        }
-    });
-};
+            let top = window.scrollY;
+            let offset = sec.offsetTop - 150;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute('id');
+
+            if (top >= offset && top < offset + height) {
+                navLinks.forEach(link => {
+                    link.classList.remove('active');
+                });
+                document.querySelector('nav a[href="#' + id + '"]').classList.add('active');
+            }
+        });
+    };
+});
+
