8db79a90de58d3896ec531bc7f7e39cca2933d54
Sofia Saraiva <112433998+Sofia-Saraiva@users.noreply.github.com>
2023-12-08 01:15:42 -0300

    transitions

--- static/script.js
+++ static/script.js
@@ -20,5 +20,19 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            console.log(entry);
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
+    });
+
+    const hiddenElements = document.querySelectorAll(".hidden");
+    hiddenElements.forEach((el) => observer.observe(el));
 });
 
